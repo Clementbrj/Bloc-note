804d8c7990aaf190fba2c7f3bfb9c048d46e12d5
Clementbrj <153804717+Clementbrj@users.noreply.github.com>
2025-07-03 15:07:31 +0200

    homescreen & edit retoucher

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,38 +1,45 @@
 class Note {
-  final String? id;  // Identifiant unique
+  final String?
+  id; // Identifiant unique (UUID pour Supabase ou int pour SQLite)
+  final String userId; // ID de l'utilisateur (nécessaire pour Supabase)
   final String title; // Titre de la note
   final String content; // Contenu de la note
-  final DateTime createdAt; // Date de création
+  final DateTime createdAt; // Date de création de la note
   final String category; // Catégorie de la note
 
-// Constructeur
+  // Constructeur
   Note({
     this.id,
+    required this.userId,
     required this.title,
     required this.content,
     required this.createdAt,
     required this.category,
   });
 
- // Convertit l'objet en Map
+  /// Convertit une Note en Map pour l'enregistrement dans la base de données
   Map<String, dynamic> toMap() => {
     'id': id,
+    'user_id': userId,
     'title': title,
     'content': content,
-    'createdAt': createdAt.toIso8601String(),
+    'created_at': createdAt.toIso8601String(),
     'category': category,
   };
 
-  // Crée un objet Note à partir d'une Map
+  /// Crée une instance de Note à partir d'une Map (depuis Supabase ou SQLite)
   factory Note.fromMap(Map<String, dynamic> map) => Note(
-      id: map['id'] as String?,
-      title: map['title'] as String,
-      content: map['content'] as String,
-      createdAt: DateTime.parse(map['created_at'] as String),
-      category: map['category'] ?? '',
+    id: map['id']?.toString(),
+    userId: map['user_id']?.toString() ?? '', // vide si SQLite local
+    title: map['title'] ?? '',
+    content: map['content'] ?? '',
+    createdAt: DateTime.parse(
+      map['created_at'] ?? map['createdAt'],
+    ), // SQLite vs Supabase
+    category: map['category'] ?? 'Sans catégorie',
   );
 
-  // Catégories autorisées
+  /// Liste des catégories disponibles
   static const List<String> allowedCategories = [
     'Low',
     'Medium',
